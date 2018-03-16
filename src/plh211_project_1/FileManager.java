ad28bb04c8c16dd49ceba6783fb20f0a5a284638
Vasilis <vskevakis@gmail.com>
2018-03-16 13:22:20 +0200

    first commit

---
+++ src/plh211_project_1/FileManager.java
@@ -0,0 +1,44 @@
+package plh211_project_1;
+
+import java.io.*;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+
+public class FileManager {
+        
+    Main main=new Main();
+
+    
+    
+    public FileManager()
+      {
+    	
+    	
+      }
+	
+	public void CreateFile() throws IOException {
+	 
+	
+	}
+	
+	public int[] FileHandle() throws IOException {
+		return null;
+		
+	}
+	
+	
+	public void WritePage(int page,int[] array) throws IOException
+	{
+		 
+	}
+	
+	
+	
+	
+	
+}
+
+
+
+
---
+++ src/plh211_project_1/Main.java
@@ -0,0 +1,37 @@
+package plh211_project_1;
+
+import java.io.*;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		RandomAccessFile MyFile = new RandomAccessFile ("my_file", "rw"); //Creating Random Access File with name my_file and read/write
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
+
